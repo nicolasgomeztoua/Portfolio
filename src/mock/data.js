c3a5a4cbfef5ec3325506039a4c54d012cd90de8
nicolasgomeztoua <nicolasnourgt@gmail.com>
2022-05-19 16:50:23 +0200

    adding new cv

--- src/mock/data.js
+++ src/mock/data.js
@@ -24,7 +24,7 @@ export const aboutData = {
     'Vengo de una variedad de orígenes. Y he estado expuesto a un ambiente multicultural toda mi vida. He pasado años de mi vida en España, Marruecos, Francia y el Reino Unido, además de graduarme de una escuela secundaria estadounidense en Marruecos.',
   paragraphThree:
     'En una industria de constante crecimiento, me preocupe principalmente de aprender las partes complejas desde el principio. Estoy orgulloso de dónde me encuentro hoy, y hago un esfuerzo constante para aprender nuevas habilidades y optimizar las actuales todos los días.',
-  resume: 'https://pdfhost.io/v/bCKbKuLkb_Nicolas_ES', // if no resume, the button will not show up
+  resume: 'https://pdfhost.io/v/~yytzQedb_newCVNicolasEN', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -33,7 +33,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'boostify.png',
     title: 'Boostify',
-    info: 'Mi mayor proyecto hasta la fecha. Una aplicación React para ayuda con los videojuegos. Usando React-Router, Styled-Components / CSS y Hooks para el Frontend. Node.js, MongoDB, Mongoose, Bycrypt, CORS y Stripe se usaron en el backend.',
+    info:
+      'Mi mayor proyecto hasta la fecha. Una aplicación React para ayuda con los videojuegos. Usando React-Router, Styled-Components / CSS y Hooks para el Frontend. Node.js, MongoDB, Mongoose, Bycrypt, CORS y Stripe se usaron en el backend.',
     info2: 'Frontend alojado con Netlify. Backend alojado en Heroku',
     url: 'https://www.boostify.es/',
     repo: 'https://github.com/nicolasgomeztoua/Boostify',
@@ -52,7 +53,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'Gatsby.png',
     title: 'Gatsby && Contentful',
-    info: 'Gracias a gatsby se elimina la neccesidad de usar systemas mas caros y lentos como wordpress y php. ',
+    info:
+      'Gracias a gatsby se elimina la neccesidad de usar systemas mas caros y lentos como wordpress y php. ',
     info2:
       'El contenido y el url de la pagina de descripcion es autogenerado gracias a Gatsby y un CMS donde el cliente puede escribir su contendio como worpdress o contentful y no tener que exponer al cliente a html y permitir a los desarolladores trbajar con mejores herramientas',
     url: 'https://reactestate.netlify.app/',
@@ -60,23 +62,12 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'Currency Convert.png',
-    title: 'Currency Convert',
-    info: 'Un proyecto simple para convertir monedas a USD que enfatiza el trabajo con una API externa. Uso de React Hooks para estructurar e interactuar con datos externos.',
-    info2:
-      'Poco o nada de CSS. Este proyecto fue una forma de familiarizarme con las promesas y el Javascript asincrónico.',
-    url: 'https://nicolasgomeztoua.github.io/CurrencyConvert/',
-    repo: 'https://github.com/nicolasgomeztoua/CurrencyConvert',
-  },
-  {
-    id: nanoid(),
-    img: 'yelpclone.png',
-    title: 'Yelp Clone',
-    info: 'Una aplicación PERN, con Psql como el foco del proyecto, para familiarizarse con los sistemas de base de datos psql y SQL.',
-    info2:
-      'Agregar, editar Eliminar y ver restaurantes en la base de datos. También cuenta con un sistema de clasificación por estrellas que utiliza las funciones AVG () y COUNT () de PSQL.',
-    url: 'https://yelpclone-react.netlify.app/',
-    repo: 'https://github.com/nicolasgomeztoua/yelpclone',
+    img: 'Pathfinder.png',
+    title: 'Buscador de Rutas',
+    info: 'Un visualizador de el algoritmo de traveso de graficas de Dijkstras. ',
+    info2: '',
+    url: 'https://pathfinder-two.vercel.app/',
+    repo: 'https://github.com/nicolasgomeztoua/Pathfinder',
   },
 ];
 
